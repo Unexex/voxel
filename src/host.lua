06369046aca3841bfa63374e71a85bc561c5b6a4
blox <72946059+coolpro200021@users.noreply.github.com>
2022-06-12 17:17:24 -0500

    changed host

--- src/host.lua
+++ src/host.lua
@@ -4,35 +4,8 @@ local parser = require "src.parser"
 local generate = require "src.generator"
 local host = {}
 
-local function CopyFile(old_path, new_path)
-	local old_file = io.open(old_path)
-	local new_file = io.open(new_path, "w")
-	if not old_file or not new_file then
-	  return false
-	end
-	while true do
-	  local block = old_file:read(2^13)
-	  if not block then break end
-	  new_file:write(block)
-	end
-	old_file:close()
-	new_file:close()
-	return true
-end
-
-local function loadLibraries(src,path)
-	local add = "\n \n"
-	CopyFile("src.delta", path..".delta")
-	for i, v in pairs(require("src.delta")) do
-		add = "var "..i.." = ".."= require(\"delta\")."..i.."\n"..add
-	end
-	
-	return add..src
-end
-
 function host:interpret(src)
-	return loadstring(host.lang:lua(src, path))
+	return src
 end
-
 
 return host
